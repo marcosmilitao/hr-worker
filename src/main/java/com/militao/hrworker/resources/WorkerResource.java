acdc09ee4919d500d7d110d00074d62b13fb397f
marcosmilitao <marcosxr7@gmail.com>
2021-09-03 21:51:17 -0300

    Hystrix fault tolerance

--- src/main/java/com/militao/hrworker/resources/WorkerResource.java
+++ src/main/java/com/militao/hrworker/resources/WorkerResource.java
@@ -32,6 +32,11 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
         Worker worker = workerRepository.findById(id).get();
